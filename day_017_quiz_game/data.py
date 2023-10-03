c8c28f580de4d14c922bd64e6cc9e36ce88abbbe
Renan Pereira <renansoaresinfo@gmail.com>
2023-10-02 21:05:29 -0300

    Dia 017 completo

---
+++ day_017_quiz_game/data.py
@@ -0,0 +1,75 @@
+question_data = [{
+    "category": "Science: Computers",
+    "type": "boolean",
+    "difficulty": "medium",
+    "question": "The HTML5 standard was published in 2014.",
+    "correct_answer": "True",
+    "incorrect_answers": ["False"]
+}, {
+    "category": "Science: Computers",
+    "type": "boolean",
+    "difficulty": "medium",
+    "question": "&quot;Windows NT&quot; is a monolithic kernel.",
+    "correct_answer": "False",
+    "incorrect_answers": ["True"]
+}, {
+    "category": "Science: Computers",
+    "type": "boolean",
+    "difficulty": "easy",
+    "question":
+    "Ada Lovelace is often considered the first computer programmer.",
+    "correct_answer": "True",
+    "incorrect_answers": ["False"]
+}, {
+    "category": "Science: Computers",
+    "type": "boolean",
+    "difficulty": "medium",
+    "question": "MacOS is based on Linux.",
+    "correct_answer": "False",
+    "incorrect_answers": ["True"]
+}, {
+    "category": "Science: Computers",
+    "type": "boolean",
+    "difficulty": "medium",
+    "question":
+    "The open source program Redis is a relational database server.",
+    "correct_answer": "False",
+    "incorrect_answers": ["True"]
+}, {
+    "category": "Science: Computers",
+    "type": "boolean",
+    "difficulty": "hard",
+    "question": "DHCP stands for Dynamic Host Configuration Port.",
+    "correct_answer": "False",
+    "incorrect_answers": ["True"]
+}, {
+    "category": "Science: Computers",
+    "type": "boolean",
+    "difficulty": "easy",
+    "question": "Time on Computers is measured via the EPOX System.",
+    "correct_answer": "False",
+    "incorrect_answers": ["True"]
+}, {
+    "category": "Science: Computers",
+    "type": "boolean",
+    "difficulty": "easy",
+    "question": "The Windows 7 operating system has six main editions.",
+    "correct_answer": "True",
+    "incorrect_answers": ["False"]
+}, {
+    "category": "Science: Computers",
+    "type": "boolean",
+    "difficulty": "medium",
+    "question":
+    "A Boolean value of &quot;0&quot; represents which of these words?",
+    "correct_answer": "False",
+    "incorrect_answers": ["True"]
+}, {
+    "category": "Science: Computers",
+    "type": "boolean",
+    "difficulty": "easy",
+    "question":
+    "The Python programming language gets its name from the British comedy group &quot;Monty Python.&quot;",
+    "correct_answer": "True",
+    "incorrect_answers": ["False"]
+}]
